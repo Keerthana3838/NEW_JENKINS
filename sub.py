81d91a4d3489a804a8540ad51a4bf809c9893f85
soumyA <hiremathsoumya@.gemail>
2025-03-19 15:26:25 +0530

    adding modified file

--- sub.py
+++
@@ -1,5 +0,0 @@
-def sub(a,b):
-    return (a-b)
-
-if __name__=='__main__':
-    print(subtractnumbers(10,5))
---
+++ subtract.py
@@ -0,0 +1,5 @@
+def sub_number(a,b):
+    return (a-b)
+
+if __name__=='__main__':
+    print(sub_number(10,5))
